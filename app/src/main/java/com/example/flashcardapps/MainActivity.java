b326a2be9c814c5016af904b3494c08a12567b8a
BingCherries <77864797+BingCherries@users.noreply.github.com>
2021-03-13 10:43:39 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/flashcardapps/MainActivity.java
+++ app/src/main/java/com/example/flashcardapps/MainActivity.java
@@ -24,5 +24,40 @@ public class MainActivity extends AppCompatActivity {
                     flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+        
+        
+        findViewById(R.id.myBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.myBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && data != null) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String String1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String String2 = data.getExtras().getString("string2");
+
+            TextView view1 = (TextView) findViewById(R.id.flashcard_question);
+            view1.setText(String1);
+
+            TextView view2 = (TextView) findViewById(R.id.flashcard_answer);
+            view2.setText(String2);
+        }
+    }
     }
 }
